47dda9d5819302ba3c0ff2016c2b0a73849e5bb3
bjmichae <joe.bowen13@gmail.com>
2026-02-01 12:57:11 -0500

    Changes

--- src/LMS.java
+++ src/LMS.java
@@ -5,117 +5,99 @@ public class LMS {
     static int bookCount = 0;
     static Scanner scanner = new Scanner(System.in);
 
-
     public static void main(String[] args) {
         boolean exit = false;
 
-        while(!exit) {
+        while (!exit) {
             System.out.println("Library Menu:");
             System.out.println("0. Exit");
             System.out.println("1. Add a Book");
             System.out.println("2. Display All Books");
             System.out.println("3. Check Out a Book");
             System.out.println("4. Return a Book");
-            System.out.print("Enter your choice: ");
-            
+            System.out.println("Enter your choice:");
 
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
-                case 0: 
+                case 0:
                     exit = true;
                     break;
-
                 case 1:
                     addBook();
                     break;
-
                 case 2:
                     displayAllBooks();
                     break;
-
                 case 3:
                     checkOutBook();
                     break;
-
                 case 4:
                     returnBook();
                     break;
-
                 default:
                     System.out.println("Invalid choice. Try again.");
+            }
+        }
 
+        System.out.println("Thank you for visiting!");
+        scanner.close();
+    }
 
+    static void addBook() {
+        if (bookCount >= 5) {
+            System.out.println("Book not added. Library is full!");
+            return;
+        }
+
+        System.out.println("Enter book title:");
+        String title = scanner.nextLine();
+
+        System.out.println("Enter book author:");
+        String author = scanner.nextLine();
+
+        books[bookCount] = new Book(bookCount + 1, title, author);
+        bookCount++;
+
+        System.out.println("Book added to library!");
+    }
+
+    static void checkOutBook() {
+        System.out.println("Enter book number to check out:");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        if (num < 1 || num > bookCount) {
+            System.out.println("Invalid book number!");
+            return;
+        }
+
+        books[num - 1].checkOut();
+    }
+
+    static void returnBook() {
+        System.out.println("Enter book number to return:");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        if (num < 1 || num > bookCount) {
+            System.out.println("Invalid book number!");
+            return;
+        }
+
+        books[num - 1].returnBook();
+    }
+
+    static void displayAllBooks() {
+        if (bookCount == 0) {
+            System.out.println("No books in library!");
+            return;
+        }
+
+        System.out.println("Books in Library:");
+        for (int i = 0; i < bookCount; i++) {
+            books[i].displayBook();
         }
     }
-    System.out.println("Thank you for visiting!");
-    scanner.close();
-}
-
-// Book Class
-static void addBook() {
-
-    // Check if the library is full
-    if (bookCount >= 5) {
-        System.out.println("Book not added. Library is full!");
-        return;
-    }
-
-    // Input book details
-    System.out.print("Enter book title: ");
-    String title = scanner.nextLine();
-
-    System.out.print("Enter book author: ");
-    String author = scanner.nextLine();
-
-    // Create and add the new Book object
-    books[bookCount] = new Book(bookCount + 1, title, author);
-    bookCount++;
-
-    System.out.println("Book added to library!");
-}
-
-// Checkout book
-static void checkOutBook() {
-    System.out.print("Enter book number to check out: ");
-    int num = scanner.nextInt();
-    scanner.nextLine();
-
-    if (num < 1 || num > bookCount) {
-        System.out.println("Invalid book number!");
-        return;
-    }
-
-    books[num - 1].checkOut();
-}
-
-// Return Book
-static void returnBook() {
-    System.out.print("Enter book number to return: ");
-    int num = scanner.nextInt();
-    scanner.nextLine();
-
-    if (num < 1 || num > bookCount) {
-        System.out.println("Invalid book number!");
-        return;
-    }
-
-    books[num - 1].returnBook();
-}
-
-// Display all Books
-static void displayAllBooks() {
-    if (bookCount == 0) {
-        System.out.println("No books in library!");
-        return;
-    }
-
-    System.out.println("Books in Library:");
-    for (int i = 0; i < bookCount; i++) {
-        books[i].displayBook();
-    }
-}
-
-
 }
