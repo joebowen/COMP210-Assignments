8db0364a74b9a8450dcde3478f55e196909b91c2
bjmichae <joe.bowen13@gmail.com>
2026-02-01 12:33:18 -0500

    assn02

---
+++ Book.java
@@ -0,0 +1,39 @@
+package assn02;
+
+public class Book {
+    int bookNumber;
+    String title;
+    String author;
+    boolean checkedOut;
+
+
+
+public Book(int bookNumber, String title, String author) {
+    this.bookNumber = bookNumber;
+    this.title = title;
+    this.author = author;
+    this.checkedOut = false;
+}
+public void displayBook() {
+    System.out.println(bookNumber + " " + title + " " + author + " " + (checkedOut ? "1" : "0"));
+}
+
+public void checkOut() {
+    if (!checkedOut) {
+        checkedOut = true;
+        System.out.println("Book checked out successfully!");
+    } else {
+        System.out.println("Book already checked out!");
+    }
+}
+
+    public void returnBook() {
+        if (checkedOut) {
+            checkedOut = false;
+            System.out.println("Book returned successfully!");
+        } else {
+            System.out.println("Book was not checked out!");
+        }
+    }
+
+}
---
+++ LMS.java
@@ -0,0 +1,125 @@
+package assn02;
+
+import java.util.Scanner;
+
+public class LMS {
+    static Book[] books = new Book[5];
+    static int bookCount = 0;
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while(!exit) {
+            System.out.println("Library Menu:");
+            System.out.println("0. Exit");
+            System.out.println("1. Add a Book");
+            System.out.println("2. Display All Books");
+            System.out.println("3. Check Out a Book");
+            System.out.println("4. Return a Book");
+            System.out.print("Enter your choice: ");
+            System.out.println("Thank you for visiting!:");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    System.out.println("See you soon :0");  
+                    exit = true;
+                    break;
+
+                case 1:
+                    addBook();
+                    break;
+
+                case 2:
+                    displayAllBooks();
+                    break;
+
+                case 3:
+                    checkOutBook();
+                    break;
+
+                case 4:
+                    returnBook();
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+
+
+        }
+    }
+
+    scanner.close();
+}
+
+// Book Class
+static void addBook() {
+
+    // Check if the library is full
+    if (bookCount >= 5) {
+        System.out.println("Book not added. Library is full!");
+        return;
+    }
+
+    // Input book details
+    System.out.print("Enter book title: ");
+    String title = scanner.nextLine();
+
+    System.out.print("Enter book author: ");
+    String author = scanner.nextLine();
+
+    // Create and add the new Book object
+    books[bookCount] = new Book(bookCount + 1, title, author);
+    bookCount++;
+
+    System.out.println("Book added to library!");
+}
+
+// Checkout book
+static void checkOutBook() {
+    System.out.print("Enter book number to check out: ");
+    int num = scanner.nextInt();
+    scanner.nextLine();
+
+    if (num < 1 || num > bookCount) {
+        System.out.println("Invalid book number!");
+        return;
+    }
+
+    books[num - 1].checkOut();
+}
+
+// Return Book
+static void returnBook() {
+    System.out.print("Enter book number to return: ");
+    int num = scanner.nextInt();
+    scanner.nextLine();
+
+    if (num < 1 || num > bookCount) {
+        System.out.println("Invalid book number!");
+        return;
+    }
+
+    books[num - 1].returnBook();
+}
+
+// Display all Books
+static void displayAllBooks() {
+    if (bookCount == 0) {
+        System.out.println("No books in library!");
+        return;
+    }
+
+    System.out.println("Books in Library:");
+    for (int i = 0; i < bookCount; i++) {
+        books[i].displayBook();
+    }
+}
+
+
+}
